f14cff3fa8b3f849d4597747350969c4d8142227
hwangsihu <hwangsihu27@gmail.com>
2025-01-01 09:24:59 +0900

    Update dependencies

--- scripts.d/25-freetype.sh
+++ scripts.d/25-freetype.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://gitlab.freedesktop.org/freetype/freetype.git"
-SCRIPT_COMMIT="38272bf85341348eb0a5162ba4e1c95d370f9bce"
+SCRIPT_COMMIT="4ef8eed11b719409d9fc284fb80d886e5bd0b28f"
 
 ffbuild_enabled() {
     return 0
