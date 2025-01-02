d35388e5900b9b4e8fbe5157d431a921f62c5772
hwangsihu <hwangsihu27@gmail.com>
2025-01-02 22:50:13 +0900

    Enable vvenc

--- scripts.d/50-vvenc.sh
+++ scripts.d/50-vvenc.sh
@@ -6,10 +6,7 @@ SCRIPT_COMMIT="81a3b6c06cfa186fa2eeccf1d8975c9ed027a214"
 ffbuild_enabled() {
     [[ $TARGET != *32 ]] || return -1
     (( $(ffbuild_ffver) > 700 )) || return -1
-    # vvenc force-enabled avx2 and equivalent compiler options, and uses a static initializer that promptly
-    # runs such instructions. Making resulting binaries malfunction on any but the very latest CPUs.
-    # Until upstream fixes this behaviour, force-disable vvenc.
-    return -1
+    return 0
 }
 
 ffbuild_dockerbuild() {
